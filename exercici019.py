9692617f98e23a5d19ebeadebaf8a75cdcce5e25
Rubén <ruben.bellido.navarro@gmail.com>
2024-11-07 12:56:33 +0100

    avances exercici019

--- exercici019.py
+++ exercici019.py
@@ -1,1 +1,46 @@
 #Arxiu exercici019.py
+import pygame
+import sys
+import utils
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((640,480))
+pygame.display.set_caption('Exercici 019')
+
+def main():
+    is_looping = True
+    
+    while is_looping:
+        is_looping = app_events()
+        app_run()
+        app_draw()
+
+        clock.tick(60)
+
+    pygame.quit()
+    sys.exit()
+
+def app_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+    return True
+
+def app_run():
+    pass
+
+def app_draw():
+    screen.fill(WHITE)
+    utils.draw_grid(pygame, screen, 50)
+
+    
+
+    pygame.display.update()
+
+if __name__ == '__main__':
+    main()

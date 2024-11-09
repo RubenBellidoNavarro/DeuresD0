fffd9a7310c5404d4c791ea42a245d9047843a66
Rubén <ruben.bellido.navarro@gmail.com>
2024-11-09 16:41:22 +0100

    realizado exercici019

--- exercici019.py
+++ exercici019.py
@@ -3,14 +3,23 @@ import pygame
 import sys
 import utils
 
-WHITE = (255,255,255)
-BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+BROWN = (165,42,42)
+GREEN = (0, 255, 0)
+BLUE  = (0, 0, 255)
 
 pygame.init()
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((640,480))
 pygame.display.set_caption('Exercici 019')
+
+moves_house = [{"direction": "right", "distance": 250}, {"direction": "up", "distance": 200}, {"direction": "left", "distance": 50}, {"direction": "up", "distance": 50}, {"direction": "left", "distance": 50}, {"direction": "up", "distance": 50}, {"direction": "left", "distance": 50}, {"direction": "down", "distance": 50}, {"direction": "left", "distance": 50}, {"direction": "down", "distance": 50}, {"direction": "left", "distance": 50}, {"direction": "down", "distance": 200}]
+moves_sun = [{"direction": "right", "distance": 25}, {"direction": "up", "distance": 25}, {"direction": "right", "distance": 25}, {"direction": "up", "distance": 25}, {"direction": "right", "distance": 50}, {"direction": "down", "distance": 25}, {"direction": "right", "distance": 25}, {"direction": "down", "distance": 25}, {"direction": "right", "distance": 25}, {"direction": "down", "distance": 50}, {"direction": "left", "distance": 25}, {"direction": "down", "distance": 25}, {"direction": "left", "distance": 25}, {"direction": "down", "distance": 25}, {"direction": "left", "distance": 50}, {"direction": "up", "distance": 25}, {"direction": "left", "distance": 25}, {"direction": "up", "distance": 25}, {"direction": "left", "distance": 25} ]
+moves_car = [{"direction": "up", "distance": 50}, {"direction": "right", "distance": 50}, {"direction": "up", "distance": 50}, {"direction": "right", "distance": 50}, {"direction": "down", "distance": 50}, {"direction": "right", "distance": 50}, {"direction": "down", "distance": 50} ]
+moves_grass = [{"direction": "right", "distance": 650}, {"direction": "down", "distance": 100}, {"direction": "left", "distance": 650}]
 
 def main():
     is_looping = True
@@ -38,9 +47,30 @@ def app_draw():
     screen.fill(WHITE)
     utils.draw_grid(pygame, screen, 50)
 
-    
+    # Dibujamos los movimientos
+    draw_moves(BROWN, (350, 400), moves_house)
+    draw_moves(YELLOW, (50, 100), moves_sun)
+    draw_moves(BLUE, (100, 400), moves_car)
+    draw_moves(GREEN, (0, 400), moves_grass)
 
     pygame.display.update()
+
+def draw_moves(color, start_pos, moves):
+    points = [start_pos]
+    for move in moves:
+        point = {'x': points[-1][0], 'y': points[-1][1]}
+        direction = move['direction']
+        distance = move['distance']
+        if direction == 'up':
+            point['y'] -= distance
+        elif direction == 'down':
+            point['y'] += distance
+        elif direction == 'right':
+            point['x'] += distance
+        elif direction == 'left':
+            point['x'] -= distance
+        points.append(tuple(point.values()))
+    pygame.draw.polygon(screen, color, points)
 
 if __name__ == '__main__':
     main()
